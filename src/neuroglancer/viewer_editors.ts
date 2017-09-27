f8fd89ef8bcea9c338a5a83bacac3b33922efd70
thejohnhoffer <hoffexi@gmail.com>
2017-09-27 15:36:58 -0400

    remove unused editor code

--- src/neuroglancer/viewer_editors.ts
+++ src/neuroglancer/viewer_editors.ts
@@ -14,43 +14,9 @@
  * limitations under the License.
  */
 
-import {ChunkManager} from 'neuroglancer/chunk_manager/frontend';
-import {DisplayContext} from 'neuroglancer/display_context';
-import {LayerManager, MouseSelectionState} from 'neuroglancer/layer';
-import {NavigationState} from 'neuroglancer/navigation_state';
-import {TrackableBoolean} from 'neuroglancer/trackable_boolean';
-import {RefCounted} from 'neuroglancer/util/disposable';
-import {VisibilityPrioritySpecification} from 'neuroglancer/viewer_state';
-
-export interface SliceViewViewerState {
-  chunkManager: ChunkManager;
-  navigationState: NavigationState;
-  layerManager: LayerManager;
-}
-
-export interface ViewerUIState extends SliceViewViewerState, VisibilityPrioritySpecification {
-  display: DisplayContext;
-  mouseState: MouseSelectionState;
-  perspectiveNavigationState: NavigationState;
-  showPerspectiveSliceViews: TrackableBoolean;
-  showAxisLines: TrackableBoolean;
-  showScaleBar: TrackableBoolean;
-}
-
-export function getCommonViewerState(viewer: ViewerUIState) {
-  return {
-    mouseState: viewer.mouseState,
-    layerManager: viewer.layerManager,
-    showAxisLines: viewer.showAxisLines,
-    visibility: viewer.visibility,
-  };
-}
-
 export function NoEditor(element: HTMLElement) {
   return `No ${element.innerText}`;
 }
-
-export interface DataEditor extends RefCounted { rootElement: HTMLElement; }
 
 export const EDITORS:
     [string, (element: HTMLElement) => string][] = [
