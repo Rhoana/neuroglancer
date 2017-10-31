f4ea94ab517ca8c3da6f5ec20e5639fd110598e5
thejohnhoffer <hoffexi@gmail.com>
2017-10-31 10:09:26 -0400

    check unsaved members of a set

--- src/neuroglancer/util/disjoint_sets.ts
+++ src/neuroglancer/util/disjoint_sets.ts
@@ -212,7 +212,7 @@ export class DisjointUint64Sets {
         let members = new Array<Uint64>();
         for (let member of setElementIterator(element)) {
           // Display only unsaved nodes
-          if (!this.isSaved(element)) {
+          if (!this.isSaved(element) || !this.isSaved(member)) {
             members.push(member);
           }
         }
